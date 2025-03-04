0599a8b4c8902e5f4438e287d5c4d78354c3a238
Miralem <miralem.banovi@89grad.ch>
2025-03-04 13:33:01 +0100

    add greet function to welcome users by name

---
+++ file.py
@@ -0,0 +1,9 @@
+# Begrüßung mit Namen 
+# Schreibe eine Funktion greet(name), die eine Person mit ihrem Namen begrüßt.
+# Beispiel: greet("Anna") → "Hallo, Anna!"
+# Variante1
+def greet(name):
+    return f"Hallo, {name}!"
+# Funktionsaufruf
+greet("Anna")
+
